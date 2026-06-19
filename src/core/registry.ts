f38a159ce224f05db810064e0fb70017ca56664a
Billuc <luc.billaud.pro@gmail.com>
2026-06-19 08:27:01 +0200

    Phase 1: create registry to be used in other phases

---
+++ src/core/registry.ts
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2026 Luc BILLAUD
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ **/
+import type { FunctionComponent } from "preact";
+
+export interface IslandEntry {
+  component: FunctionComponent<any>;
+  hash: string;
+  scriptPath: string;
+  publicPath: string;
+}
+
+const islandComponentMap = new Map<FunctionComponent<any>, IslandEntry>();
+
+export function setIslandMap(entries: IslandEntry[]) {
+  islandComponentMap.clear();
+  for (const entry of entries) {
+    islandComponentMap.set(entry.component, entry);
+  }
+}
+
+export function getIslandEntry<T>(
+  component: FunctionComponent<T>,
+): IslandEntry | undefined {
+  return islandComponentMap.get(component);
+}
+
+const assetRoutes = new Map<string, string>();
+
+export function addAssetRoute(publicPath: string, absolutePath: string) {
+  assetRoutes.set(publicPath, absolutePath);
+}
+
+export function getAssetRoutes(): ReadonlyMap<string, string> {
+  return assetRoutes;
+}
