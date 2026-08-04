68eb0ad438f3c1972291eb940fe2459bd72d23ab
Billuc <luc.billaud.pro@gmail.com>
2026-08-04 19:12:26 +0200

    chore: added copyrights

--- src/core/context.ts
+++ src/core/context.ts
@@ -1,4 +1,24 @@
-import { createContext, h, type ComponentChildren, type FunctionComponent } from "preact";
+/**
+ * Copyright 2026 Luc BILLAUD
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ **/
+import {
+  createContext,
+  h,
+  type ComponentChildren,
+  type FunctionComponent,
+} from "preact";
 import type { IslandEntry } from "./registry";
 
 export const BaseContext = createContext("");
@@ -13,7 +33,9 @@ export function BaseProvider({
   return h(BaseContext.Provider, { value }, children);
 }
 
-export const IslandMapContext = createContext<Map<FunctionComponent<any>, IslandEntry>>(new Map());
+export const IslandMapContext = createContext<
+  Map<FunctionComponent<any>, IslandEntry>
+>(new Map());
 
 export function IslandMapProvider({
   entries,
