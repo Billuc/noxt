6bf58eb40dfb6a68e03f2adf40de4fe1073abfbf
Billuc <billuc@hotmail.fr>
2026-09-08 15:03:22 +0200

    feat: Keep a server running and only reload it when you need

--- examples/demo/dev.ts
+++ examples/demo/dev.ts
@@ -2,6 +2,70 @@ import type { FileChangeInfo } from "node:fs/promises";
 import * as fs from "node:fs/promises";
 
 let abortController = new AbortController();
+let server: Bun.Server<any>;
+
+// TODO : creer un Server Builder qui permet de créer un serveur de dev ou de prod
+class AbortError extends Error {}
+
+async function buildProject(): Promise<void> {
+  abortController.abort();
+  abortController = new AbortController();
+
+  console.log("Building...");
+  const buildStep = Bun.spawn({
+    cmd: ["bun", "./build.ts"],
+    stderr: "inherit",
+    stdout: "inherit",
+    signal: abortController.signal,
+  });
+  const buildCode = await buildStep.exited;
+
+  if (buildStep.signalCode === "SIGTERM") {
+    throw new AbortError("Aborted !");
+  }
+  if (buildCode !== 0) {
+    throw new Error("Build failed !");
+  }
+}
+
+async function importRoutes(): Promise<Bun.Serve.Routes<any, any>> {
+  const { handlers } = await import(`./.cache/api.ts?t=${Bun.nanoseconds()}`);
+  const routeMap = (await Bun.file("./.cache/routes.json").json()) as Record<
+    string,
+    string
+  >;
+
+  const routes: Record<string, Response> = {};
+  for (const [url, file] of Object.entries(routeMap)) {
+    routes[url] = new Response(Bun.file(file));
+  }
+
+  return {
+    ...handlers,
+    ...routes,
+  };
+}
+
+async function startServer(base: string, port: number) {
+  const routes = await importRoutes();
+
+  const server = Bun.serve({
+    port,
+    routes,
+    development: true,
+  });
+
+  console.log(
+    `Demo serving at http://localhost:${port}${base} (${Object.keys(routes).length} routes)`,
+  );
+  return server;
+}
+
+async function restartServer(server: Bun.Server<any>) {
+  const routes = await importRoutes();
+
+  return server.reload({ routes });
+}
 
 function debounceEvent<T>(
   onEvent: (ev: T) => Promise<any> | any,
@@ -20,39 +84,27 @@ function debounceEvent<T>(
   };
 }
 
-async function rebuildAndRestart() {
-  abortController.abort();
-
-  console.log("Building...");
-  const buildStep = Bun.spawn({
-    cmd: ["bun", "./build.ts"],
-    stderr: "inherit",
-    stdout: "inherit",
-  });
-  const buildCode = await buildStep.exited;
-
-  if (buildCode === 0) {
-    abortController = new AbortController();
-    console.log("Starting the server...");
-    Bun.spawn({
-      cmd: ["bun", "./serve.ts"],
-      stderr: "inherit",
-      stdout: "inherit",
-      signal: abortController.signal,
-    });
-  }
-}
-
 async function onFileChange(fc: FileChangeInfo<string>) {
   console.log(
     fc.filename +
       " changed ! Rebuilding the project and restarting the server...",
   );
-  await rebuildAndRestart();
+  try {
+    await buildProject();
+  } catch (err) {
+    if (!(err instanceof AbortError)) throw err;
+    return;
+  }
+  await restartServer(server);
 }
 
 async function startWatch() {
-  await rebuildAndRestart();
+  const base = Bun.env.NOXT_BASE ?? "";
+  const port = Number(Bun.env.PORT ?? 3000);
+
+  await buildProject();
+  server = await startServer(base, port);
+
   const watcher = fs.watch("src", {
     recursive: true,
   });
@@ -63,5 +115,5 @@ async function startWatch() {
   }
 }
 
-startWatch();
 console.log("Dev server started !\n");
+startWatch();
